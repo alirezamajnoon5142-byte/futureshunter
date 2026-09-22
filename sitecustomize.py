e76cd015e84ce452007d19e690d67fc9594b9e70
alirezamajnoon5142-byte <alireza.majnoon5142@gmail.com>
2026-09-22 18:04:37 +0200

    Activate V7.8.6 range transport throttle

--- sitecustomize.py
+++ sitecustomize.py
@@ -9,4 +9,5 @@ except ModuleNotFoundError:
     pass
 else:
     from v786_range_overlay import *  # noqa: F401,F403
+    import v786_range_transport_guard  # noqa: F401
     import v786_range_hardening  # noqa: F401
